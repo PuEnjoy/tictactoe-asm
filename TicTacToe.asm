89bf56c2c2277187876f38d03ac7e544a1095ba6
Enjoy <enjoy@fedora.fritz.box>
2024-08-20 19:15:56 +0200

    improve comment documentation

--- TicTacToe.asm
+++ TicTacToe.asm
@@ -1,6 +1,6 @@
 ;data section
 section .data
-    grid db ".",9,".",9,".",10,".",9,".",9,".",10,".",9,".",9,".",10
+    grid db ".",9,".",9,".",10,".",9,".",9,".",10,".",9,".",9,".",10    ;build simple grid with tab (9) and linebreak (10) seperations
     gridlen: equ $ - grid   ;grid length: equate difference from grid to current position
     
 ;read only data section
@@ -19,139 +19,126 @@ section .rodata
     placeTakenLen: equ $ - placeTaken
 
     
-
+;block starting symbol - uninitialised data section. Initialized to 0 on startup. Modified during runtime
 section .bss
-    inputBuffer resb 256
+    inputBuffer resb 256            ;initialize buffer to 256 byte even tho only 1 is used (prevent accidental command execution)
 
 ;main code section
 section .text
-global _start ;declare _start as global
+global _start                       ;declare _start as global
 
-;syscall setup for a simple stdout
 _start:
-    mov r12, "X"
-    call _printIntro
-    ;print ask for input
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [askInputMsgX]
-    mov rdx, askInputMsgXLen
-    syscall
-    call _get_input
-    call _print_grid
-    call _get_input
-    call _print_grid
-    call _get_input
-    call _print_grid
-    jmp _exit
+    mov r12, "X"                    ;use non-volatile register r12 to store whos turn it is.
+    call _printIntro                ;
+    mov rax, 1                      ;Setup syscall to ask for user input (only print)
+    mov rdi, 1                      ;
+    lea rsi, [askInputMsgX]         ;
+    mov rdx, askInputMsgXLen        ;
+    syscall                         ;syscall
+    call _get_input                 ;function call to handle user input
+    call _print_grid                ;
+    call _get_input                 ;
+    call _print_grid                ;
+    call _get_input                 ;
+    call _print_grid                ;
+    jmp _exit                       ;jump to program no error exit function
 
 _get_input:
-    mov rax, 0
-    mov rdi, 1
-    lea rsi, [inputBuffer]
-    mov rdx, 256
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [placeholer]
-    mov rdx, placeholerLen
-    syscall
-    call _placeSymbol
-    test rax, rax
-    jz .changeSymbol
-    jmp _get_input
-    ;this get executed even when teh place was taken! Needs fix
-    .changeSymbol:
-        test r12, 1
-        je .changeToO
-        jmp .changeToX
-    .changeToX:
-        add r12, 9
-        ret
-    .changeToO:
-        sub r12, 9
-        ret
+    mov rax, 0                      ;syscall setup to take user input (stdin)
+    mov rdi, 1                      ;
+    lea rsi, [inputBuffer]          ;
+    mov rdx, 256                    ;large inputBuffer to prevent accidental overflows which can lead to command execution
+    syscall                         ;syscall
+    mov rax, 1                      ;aesthetic print syscall
+    mov rdi, 1                      ;
+    lea rsi, [placeholer]           ;
+    mov rdx, placeholerLen          ;
+    syscall                         ;exection of syscall
+    call _placeSymbol               ;
+    test rax, rax                   ;test rax on rax -> return code 0 (No Error), 1 (Error)
+    jz .changeSymbol                ;call .changeSymbol if no error occured
+    jmp _get_input                  ;start _get_input loop again in case of error
+
+    .changeSymbol:                  ;subFunction
+        test r12, 1                 ;test r12 odd or even
+        je .changeToO               ;even test mean current symbol is X (ascii 88) -> change to O
+        jmp .changeToX              ;odd test implies current symbol is O (ascii 79) -> change to X
+    .changeToX:                     ;
+        add r12, 9                  ;change symbol by adding 9 on ascii code
+        ret                         ;
+    .changeToO:                     ;
+        sub r12, 9                  ;change symbol by subtracting 9 from ascii code
+        ret                         ;
 
 
 _placeSymbol:
-    ;check if the position is already taken
-    xor rax, rax
-    mov al, byte [inputBuffer]
-    sub al, '0' ;convert ascii to number    
-    ;multiply input value by 2
-    mov rsi, 2
-    mul rsi
-    ;subtrackt 2
-    sub rax, 2
-    ;at this point we have the grid adress correlating to the input saved in rax (not handling wrong inputs yet!!!) 
-    xor rdi, rdi
-    mov rdi, "."
-    mov dl, byte [grid+rax]
-    cmp dl, dil
-    jne .placeTakenError
-    ;place Symbol at location:
-    mov byte [grid+rax], r12b
-    xor rax, rax
-    ret
+    ;Here we convert the user input into the corresponding position of our game grid
+    xor rax, rax                    ;zero rax
+    mov al, byte [inputBuffer]      ;move first byte from input buffer into lower 8bit of rax
+    sub al, '0'                     ;convert ascii to integer    
+    mov rsi, 2                      ;
+    mul rsi                         ;mul input by 2
+    sub rax, 2                      ;sub result by 2
+    ;At this point the lower 8bit of rax contain the selected location of the grid as we can use it to navigate the grid data  
+    xor rdi, rdi                    ;
+    mov rdi, "."                    ;
+    mov dl, byte [grid+rax]         ;get the symbol at the selected location
+    cmp dl, dil                     ;compare selected symbol to "." / 46
+    jne .placeTakenError            ;jmp if selected symbol is not free "."
+    mov byte [grid+rax], r12b       ;place active symbol at grid location
+    xor rax, rax                    ;
+    ret                             ;no error return
 
-    .placeTakenError:
-        mov rax, 1
-        mov rdi, 1
-        lea rsi, [placeTaken]
-        mov rdx, placeTakenLen
-        syscall
-        mov rax, 1 ;indicate error
-        jmp _get_input
+    .placeTakenError:               ;
+        mov rax, 1                  ;syscall setup to print place taken error message
+        mov rdi, 1                  ;
+        lea rsi, [placeTaken]       ;
+        mov rdx, placeTakenLen      ;
+        syscall                     ;
+        mov rax, 1                  ;indicate error
+        jmp _get_input              ;return to get input loop
 
 
 _printIntro:
-    ;print welcome message
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [welcomeMsg]
-    mov rdx, welcomeMsgLen
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [placeholer]
-    mov rdx, placeholerLen
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [gridIntro]
-    mov rdx, gridIntroLen
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [placeholer]
-    mov rdx, placeholerLen
-    syscall
-    ret
+    mov rax, 1                      ;syscall setup to print welcome message
+    mov rdi, 1                      ;
+    lea rsi, [welcomeMsg]           ;
+    mov rdx, welcomeMsgLen          ;
+    syscall                         ;
+    mov rax, 1                      ;syscall setup to print placeholder
+    mov rdi, 1                      ;
+    lea rsi, [placeholer]           ;
+    mov rdx, placeholerLen          ;
+    syscall                         ;
+    mov rax, 1                      ;syscall setup to print grid intro
+    mov rdi, 1                      ;
+    lea rsi, [gridIntro]            ;
+    mov rdx, gridIntroLen           ;
+    syscall                         ;
+    mov rax, 1                      ;syscall setup to print placeholder
+    mov rdi, 1                      ;
+    lea rsi, [placeholer]           ;
+    mov rdx, placeholerLen          ;
+    syscall                         ;
+    ret                             ;return 
 
-
-_print_msg:
-    mov rax, 1
-    mov rdi, 1
-    ;set rsi and rdx before function call
-    syscall
-    ret
 
 ;function handling simple syscall setup for stdout (used for printing the grid)
 _print_grid:
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [grid]
-    mov rdx, gridlen
-    syscall
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [placeholer]
-    mov rdx, placeholerLen
-    syscall
-    ret
+    mov rax, 1                      ;syscall setup to print the grid
+    mov rdi, 1                      ;formatting happens in datasection
+    lea rsi, [grid]                 ;
+    mov rdx, gridlen                ;
+    syscall                         ;
+    mov rax, 1                      ;syscall setup to print placeholder
+    mov rdi, 1                      ;
+    lea rsi, [placeholer]           ;
+    mov rdx, placeholerLen          ;
+    syscall                         ;
+    ret                             ;
 
 ;syscall setup to exit the program voluntarily without error 
 _exit:
-    mov rax, 60
-    mov rdi, 0
-    syscall
+    mov rax, 60                     ;sys_exit code
+    mov rdi, 0                      ;no error
+    syscall                         ;execute syscall
